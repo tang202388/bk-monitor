e2186c7326b96dd765f5f7914daf940dc623a215
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2026-03-18 16:48:18 +0800

    feat: 完成Issues快捷筛选开发以及完善issues详情问题活动 (#10024)

--- bkmonitor/webpack/src/trace/pages/alarm-center/alarm-center.tsx
+++ bkmonitor/webpack/src/trace/pages/alarm-center/alarm-center.tsx
@@ -117,6 +117,7 @@ export default defineComponent({
       updateQuickFilterValue,
       handleQuickFilteringOperation,
     } = useQuickFilter();
+
     const { data, loading, total, page, pageSize, ordering } = useAlarmTable();
     const {
       tableColumns: tableSourceColumns,
@@ -270,6 +271,12 @@ export default defineComponent({
         currentFavorite.value = null;
       }
     );
+
+    /** 告警类型切换 */
+    const handleAlarmTypeChange = (value: AlarmType) => {
+      alarmStore.handleAlarmTypeChange(value);
+      isFirstInit.value = true;
+    };
 
     const updateIsCollapsed = (v: boolean) => {
       isCollapsed.value = v;
@@ -810,6 +817,7 @@ export default defineComponent({
       handleDetailShowChange,
       handlePreviousDetail,
       handleNextDetail,
+      handleAlarmTypeChange,
       handleFavoriteShowChange,
       handleFavoriteSave,
       handleEditFavoriteShow,
@@ -858,6 +866,7 @@ export default defineComponent({
           <AlarmCenterHeader
             class='alarm-center-header'
             isShowFavorite={this.isShowFavorite}
+            onAlarmTypeChange={this.handleAlarmTypeChange}
             onFavoriteShowChange={this.handleFavoriteShowChange}
           />
           <AlarmRetrievalFilter
